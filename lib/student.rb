e933351ce08b4ea84721244903f988005136c110
Cristina Murillo <murillocam96@gmail.com>
2018-09-27 10:29:14 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,20 +1,72 @@
+require 'pry'
 class Student
   attr_accessor :id, :name, :grade
 
   def self.new_from_db(row)
     # create a new Student object given a row from the database
+    student = Student.new 
+    student.id = row[0]
+    student.name = row[1]
+    student.grade = row [2]
+
+    student 
   end
 
   def self.all
     # retrieve all the rows from the "Students" database
     # remember each row should be a new instance of the Student class
+    sql = <<-SQL
+      SELECT * FROM students 
+    SQL
+
+    DB[:conn].execute(sql).collect do |row|
+      self.new_from_db(row)
+    end
   end
 
   def self.find_by_name(name)
     # find the student in the database given a name
     # return a new instance of the Student class
+
+    sql = <<-SQL
+      SELECT * FROM students WHERE students.name = name 
+    SQL
+
+    row = DB[:conn].execute(sql).flatten 
+    self.new_from_db(row)
+
   end
-  
+
+  def self.all_students_in_grade_9
+    self.all.select do |student|
+      student.grade == "9"
+    end
+  end
+
+  def self.students_below_12th_grade
+    self.all.select do |student|
+      student.grade != "12"
+    end
+  end
+
+  def self.first_X_students_in_grade_10(x)
+    self.all.select do |student|
+      student.grade == "10"
+    end.first(x)
+  end
+
+  def self.first_student_in_grade_10 
+    self.all.select do |student|
+      student.grade == "10"
+    end.first
+  end 
+
+  def self.all_students_in_grade_X(x)
+    self.all.select do |student|
+      student.grade == x.to_s
+    end
+  end
+
   def save
     sql = <<-SQL
       INSERT INTO students (name, grade) 
